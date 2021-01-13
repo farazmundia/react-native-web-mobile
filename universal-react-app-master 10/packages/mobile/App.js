1f4a0f4e0b3f7ffd663e44ff8ac3e7e34612569a
js <Rajkumar.thulasimani@7-11.com>
2021-01-13 11:43:02 -0600

    navigation

---
+++ universal-react-app-master 10/packages/mobile/App.js
@@ -0,0 +1,36 @@
+//import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from '@universal/common/store';
+
+import Home from '@universal/common/containers/Home';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+
+
+export default function App() {
+  return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Home" component={HomeScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+}
---
+++ universal-react-app-master 10/packages/web/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "@universal/common/store";
+
+import Home from "@universal/common/containers/Home";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+}
